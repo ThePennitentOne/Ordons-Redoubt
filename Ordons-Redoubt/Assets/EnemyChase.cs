176977aefabedf1173dccf1d77173d0499e32101
Michał Dyło <dylomichal@gmail.com>
2024-03-10 21:19:19 +0100

    Added enemy follow

--- Ordons-Redoubt/Assets/EnemyChase.cs
+++ Ordons-Redoubt/Assets/EnemyChase.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class EnemyChase : MonoBehaviour
 {
+    public GameObject player;
+    public float speed = 1f;
+    private float distance;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,8 @@ public class EnemyChase : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+        transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
     }
 }
