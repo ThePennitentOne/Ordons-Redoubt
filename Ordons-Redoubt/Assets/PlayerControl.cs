98f5d96b65241c1bfee8969236b3a2ae3fefd672
Michał Dyło <dylomichal@gmail.com>
2024-03-10 16:04:02 +0100

    Added player controls

--- Ordons-Redoubt/Assets/PlayerControl.cs
+++ Ordons-Redoubt/Assets/PlayerControl.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    public float MoveSpeed = 2f;
+    float speedX, speedY;
+    Rigidbody2D playerRigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        speedX = Input.GetAxisRaw("Horizontal") * MoveSpeed;
+        speedY = Input.GetAxisRaw("Vertical") * MoveSpeed;
+        playerRigidbody.velocity = new Vector2(speedX, speedY);
     }
 }
