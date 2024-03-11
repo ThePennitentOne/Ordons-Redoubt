c850b6d81e9bae9ad911af179eb8ae0073fc04de
Michał Dyło <dylomichal@gmail.com>
2024-03-11 03:49:39 +0100

    Tried to fix ammo spawn on cannon

--- Ordons-Redoubt/Assets/Scripts/AmmoPackScript.cs
+++ Ordons-Redoubt/Assets/Scripts/AmmoPackScript.cs
@@ -24,16 +24,19 @@ public class AmmoPackScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag.Equals("RotatePoint"))
-        {
-            ammoSpawn.SpawnAmmoPack();
-            Destroy(gameObject);
-        }
-
         if (collision.gameObject.tag.Equals("Player") && ammo.ammoCount == 0)
         {
             ammo.IncreaseAmmo(4);
             ammoSpawn.ammoPackExists = false;
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.Equals("RotatePoint"))
+        {
+            ammoSpawn.SpawnAmmoPack();
             Destroy(gameObject);
         }
     }
